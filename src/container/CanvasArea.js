6ee2461ea0b3ed2cdbaf2a5af7b6f1aa1f7051f4
yyijoo <yyijoo@gmail.com>
2019-06-17 19:25:47 +0900

    feat: Make CanvasArea and Canvas

--- src/container/CanvasArea.js
+++ src/container/CanvasArea.js
@@ -1,8 +1,66 @@
-import React from "react";
-import { ToolBox } from "components/Boxes";
+import React, { Component } from "react";
+import { ToolBox, CanvasBox, Canvas } from "components/Boxes";
+import { connect } from "react-redux";
+import { selectShape, draw } from "redux/action/drawAction";
 
-const CanvasArea = () => {
-  return <ToolBox>CanvasArea</ToolBox>;
+class CanvasArea extends Component {
+  drawLine = (x1, y1, x2, y2) => {
+    this.props.coordinate.line = (
+      <line x1={x1} y1={y1} x2={x2} y2={y2} stroke="orange" stroke-width="5" />
+    );
+  };
+
+  saveLine = () => {
+    this.props.coordinate.shapes.push(this.props.coordinate.line);
+    this.props.coordinate.line = null;
+  };
+
+  componentDidUpdate = () => {
+    const { x1, y1, x2, y2, shapeIsSelected } = this.props.coordinate;
+    if (shapeIsSelected) this.drawLine(x1, y1, x2, y2);
+  };
+  render() {
+    const { shapes, line } = this.props.coordinate;
+    const { draw, selectShape } = this.props;
+
+    return (
+      <CanvasBox>
+        CanvasArea
+        <Canvas
+          onMouseDown={e => {
+            draw("start", e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+          }}
+          onMouseMove={e => {
+            draw("end", e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+          }}
+          onMouseUp={() => {
+            this.saveLine();
+            selectShape(null);
+          }}
+        >
+          canvas
+        </Canvas>
+      </CanvasBox>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    draw: (startOrEnd, x, y) => dispatch(draw(startOrEnd, x, y)),
+    selectShape: () => dispatch(selectShape())
+  };
 };
 
-export default CanvasArea;
+const mapStateToProps = state => {
+  return {
+    coordinate: state.drawReducer
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CanvasArea);
+
+// export default CanvasArea;
