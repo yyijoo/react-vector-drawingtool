9fca436d6a872f92f952837f665bab0b86106e3f
yyijoo <yyijoo@gmail.com>
2019-06-18 09:02:45 +0900

    feat: Make select icon to grab the shape

--- src/container/ActionTool.js
+++ src/container/ActionTool.js
@@ -1,8 +1,38 @@
 import React from "react";
 import { ToolBox } from "component/Boxes";
+import selectIcon from "select.png";
+import styled from "styled-components";
+import { selectShape, getCoord, draw } from "redux/action/drawAction";
+import { connect } from "react-redux";
 
-const ActionTool = () => {
-  return <ToolBox>Action Tool</ToolBox>;
+const SelectIcon = styled.img`
+  width: 30px;
+  border: 1px yellow solid;
+`;
+
+const ActionTool = ({ selectShape }) => {
+  return (
+    <ToolBox>
+      Action Tool
+      <SelectIcon src={selectIcon} onClick={() => selectShape(null)} />
+    </ToolBox>
+  );
 };
 
-export default ActionTool;
+// const mapStateToProps = state => {
+//   const {selectShape} = state.drawReducer
+//   return {
+//     selectShape:
+//   }
+// }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    selectShape: () => dispatch(selectShape(null))
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(ActionTool);
