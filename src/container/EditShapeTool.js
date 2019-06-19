23a3353dc68dbfe802b32503d17fc4bee8cb45f7
yyijoo <yyijoo@gmail.com>
2019-06-19 22:14:47 +0900

    refactor: Separate ColorOpacityController into independant component

--- src/container/EditShapeTool.js
+++ src/container/EditShapeTool.js
@@ -1,65 +1,18 @@
 import React, { Component } from "react";
 import {
   ToolBox,
-  ToolBoxTitles,
   ToolBoxLeft,
   ToolBoxTitle,
   ToolItem,
   ToolItemTitle,
   ToolItemValue,
-  Item,
   Input
 } from "component/Boxes";
+import ColorOpacController from "component/ColorOpacController";
 
 import { connect } from "react-redux";
-import styled from "styled-components";
-import { BlockPicker } from "react-color";
+
 import { editColorFill, editColorStroke } from "redux/action/editShapeAction";
-
-class ColorOpacController extends Component {
-  state = {
-    showPicker: false
-  };
-
-  render() {
-    const { showPicker } = this.state;
-    const { color, opacity, editColor } = this.props;
-
-    const ColorPickerButton = styled.span`
-      display: inline-block;
-      background-color: ${props => (props.color ? props.color : "")};
-      width: 15px;
-      height: 15px;
-      border: 1px solid black;
-      position: relative;
-    `;
-
-    const ColorPicker = ({ color, onChange }) => {
-      const PickerBox = styled.div`
-        position: absolute;
-        top: 30px;
-        left: -77px;
-      `;
-
-      return (
-        <PickerBox>
-          <BlockPicker color={color} onChange={onChange} />
-        </PickerBox>
-      );
-    };
-    return (
-      <Item>
-        <ColorPickerButton
-          color={color}
-          onClick={() => this.setState({ showPicker: !showPicker })}
-        >
-          {showPicker ? <ColorPicker color={color} onChange={editColor} /> : ""}
-        </ColorPickerButton>
-        <Input value={opacity} />
-      </Item>
-    );
-  }
-}
 
 class EditShapeTool extends Component {
   render() {
