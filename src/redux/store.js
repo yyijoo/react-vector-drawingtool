60655dbc2da12989f1b8c32cd73299becf389bfd
yyijoo <yyijoo@gmail.com>
2019-06-22 20:30:30 +0900

    feat: Pass redux devtool into store only when it exists not to cause error

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import rootReducer from "redux/reducer";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers()
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
